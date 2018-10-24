73d47e78a4c57fbe77ff098e31513bb495483505
OranAfekaTest <oracohen91@gmail.com>
2018-10-24 20:28:14 +0300

    #all buttons working and communicating with server

--- StoreProject/src/main/java/com/amazonaws/samples/view/Controller.java
+++ StoreProject/src/main/java/com/amazonaws/samples/view/Controller.java
@@ -16,23 +16,21 @@ import javafx.scene.control.TextField;
 
 public class Controller {
 	
-	
+	// static names for DynamoDBHandler
 	static String region = "us-east-2";
-	
-	// columns name of tables
 	static String orderId = "orderId";
 	static String orderContent = "orderContent";
 	static String orderStatus = "status";
-	
 	static String items_table_name = "items_table";
-	
 	static String category = "category";
 	static String items = "items";
-			
-			//tables names
-			static String order_table_name = "order_table";
+	static String order_table_name = "order_table";
+	static String order_status = "undone";
+	static String order_keyGen = "0";
 	
-	DynamoDBHandler myDynamoDBHandler;
+	DynamoDBHandler itemsDynamoDBHandler;
+	DynamoDBHandler ordersDynamoDBHandler;
+	
 	ObservableList<String> categoriesList = FXCollections.observableArrayList("alcohol", "drinks");
 	
 	private HashMap<String, ObservableList<String>> map = new HashMap<String, ObservableList<String>>();
@@ -53,8 +51,8 @@ public class Controller {
 	@FXML
 	private void initialize() {
 		
-		myDynamoDBHandler = new DynamoDBHandler(region, items_table_name, null,category,items);
-		
+		itemsDynamoDBHandler = new DynamoDBHandler(region, items_table_name, null,category,items);
+		ordersDynamoDBHandler = new DynamoDBHandler(region, order_table_name, null,orderId,orderContent,orderStatus);
 		categoriesBox.setItems(categoriesList);
 		setMap();
 		
@@ -72,11 +70,27 @@ public class Controller {
 	private void add() {
 		try {
 			int qnt = Integer.parseInt(qntField.getText());
-			if(productsBox.getValue()==null)
-				throw new InvalidInputException(productsBox.getValue());
+			String prodName = productsBox.getValue();
 			
-			String toList = productsBox.getValue() + " " + qnt;
+			if(prodName == null)
+				throw new InvalidInputException(prodName);
+			// check if item already exists in the list, if it does update the quantity
+			for (String str : prodList.getItems()) {
+				String[] item = str.split(" ");
+				System.out.println("now in item - " + str);
+				if (item[0].equals(prodName)) {
+					System.out.println(prodName + " already exists in the list");
+					qnt += Integer.parseInt(item[1]);
+					System.out.println("qnt updated");
+					prodList.getItems().remove(str);
+					System.out.println(str + " was removed");
+					break;
+				}
+			}
+			
+			String toList = prodName + " " + qnt;
 			prodList.getItems().add(toList);
+			System.out.println(toList + " was added to list");
 			
 		}
 		catch(NumberFormatException e) {
@@ -88,25 +102,53 @@ public class Controller {
 		catch(InvalidInputException e) {
 			Alert alert = new Alert(AlertType.ERROR);
 			alert.setTitle("Error!");
-			alert.setContentText("you nust choose a product first");
+			alert.setContentText("You Must choose a product first");
 			alert.showAndWait();
 		}
 	}
 	
 	@FXML
 	private void buy() {
+		String value = "";
+		String key;
+		int order_id = Integer.parseInt(ordersDynamoDBHandler.retrieveItemString(order_keyGen));
+		System.out.println("current order keyGen at DB is: " + order_id);
 		
+		if(prodList.getItems().isEmpty()) {
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setTitle("Error!");
+			alert.setContentText("Product list is empty!");
+			alert.showAndWait();
+			return;
+		}
+		
+		for (String str : prodList.getItems()) {
+			value += str + ",";
+		}
+		
+		key = "" + (++order_id);
+		
+		ordersDynamoDBHandler.putOrderToTable(key, value, order_status);
+		ordersDynamoDBHandler.putOrderToTable(order_keyGen, key, "keyGen");
+		
+		Alert alert = new Alert(AlertType.INFORMATION);
+		alert.setTitle("Tank you!");
+		alert.setContentText("Your order has been submited!");
+		alert.showAndWait();
+		
+		prodList.getItems().clear();
 	}
 	
 	@FXML
 	private void delete() {
+		prodList.getItems().removeAll(prodList.getSelectionModel().getSelectedItems());
 		
 	}
 	
 	private void setMap() {
 		 //When server is up
 		for (String cat : categoriesList) {
-			String str = myDynamoDBHandler.retrieveItemString(cat);
+			String str = itemsDynamoDBHandler.retrieveItemString(cat);
 			ObservableList<String> productList = FXCollections.observableArrayList(str.split(","));
 			map.put(cat, productList);
 		}
